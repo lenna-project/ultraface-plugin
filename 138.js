df40e441afb2957d53dc267914a5dc6233f6a3ea
chriamue <chriamue@users.noreply.github.com>
2021-12-29 16:43:00 +0000

    deploy: 023f5f1c982b2119f89f5efbd0014c0e01f7084a

---
+++ 138.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunk_lenna_project_ultraface"] = self["webpackChunk_lenna_project_ultraface"] || []).push([[138,179],{
+
+/***/ 138:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "processor": () => (/* binding */ processor),
+/* harmony export */   "name": () => (/* binding */ name),
+/* harmony export */   "description": () => (/* binding */ description),
+/* harmony export */   "process": () => (/* binding */ process),
+/* harmony export */   "defaultConfig": () => (/* binding */ defaultConfig)
+/* harmony export */ });
+const pkg = __webpack_require__.e(/* import() */ 976).then(__webpack_require__.bind(__webpack_require__, 976));
+const processor = pkg;
+const name = () => "ultraface";
+const description = () => "Plugin to label images.";
+const process = async (config, image) => {
+  return __webpack_require__.e(/* import() */ 976).then(__webpack_require__.bind(__webpack_require__, 976)).then((processor) => processor.process(config, image));
+};
+const defaultConfig = async () => {
+  return {};
+};
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=138.js.map
