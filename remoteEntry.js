4226b9eb4eb1be1f44858bc94883a004eb74a076
chriamue <chriamue@users.noreply.github.com>
2022-01-03 11:53:42 +0000

    deploy: fb3df495c8c097426a049763bc955e9e9947f195

--- remoteEntry.js
+++ remoteEntry.js
@@ -402,7 +402,7 @@ __webpack_require__.d(exports, {
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"138":{"503":"0edbca2b912088744e2f"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"138":{"503":"9b0414417b5ff1d6511b"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject && typeof importObject.then === 'function' && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
